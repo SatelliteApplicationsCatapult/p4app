f994013f15204ce84d3b76823337e1b754114c0b
Juan Emilio Zurita Macías <juan.macias@sa.catapult.org.uk>
2022-08-22 12:36:15 +0200

    [OR-16] Setup P4 testing environment (#1)
    * Customise p4app with personalised outputs in logs
    
    * Modify dockerfile for supporting custom scripts
    
    * Add scp message to logs

--- docker/scripts/p4apprunner.py
+++ docker/scripts/p4apprunner.py
@@ -175,6 +175,15 @@ def run_mininet(manifest):
         print('To run the switch debugger, run this command from your host OS:', file=message)
         print('  docker exec -t -i %s bm_p4dbg' % container, file=message)
         print(file=message)
+        print('To execute a bash session from h1, run this command from your host OS:', file=message)
+        print('  docker exec -t -i %s m h1 bash' % container, file=message)
+        print(file=message)
+        print('To execute a bash session from h2, run this command from your host OS:', file=message)
+        print('  docker exec -t -i %s m h2 bash' % container, file=message)
+        print(file=message)
+        print('To copy the compiled files to P4 machine:', file=message)
+        print('  docker exec -t -i %s bash -c \'/usr/bin/scp /tmp/*.json p4@10.30.150.217:/tmp/.\'' % container, file=message)
+        print(file=message)
 
     switch_args.append('--cli-message "%s"' % message_file)
 
